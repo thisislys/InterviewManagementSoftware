418a4102f25c04276de643836d178f71dd52bab9
KangJT <1325770470@qq.com>
2019-03-21 14:52:27 +0800

    头部横滚

--- lyApp/src/pages/logs/index.vue
+++ lyApp/src/pages/logs/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-     
-  </div>
+  <header>
+      <view class="section section_gap">
+          <scroll-view class="scroll-view_H" scroll-x style="width: 100%">
+            <text>首页</text>
+            <text>美妆</text>
+            <text>首页</text>
+            <text>首页</text>
+            <text>首页</text>
+            <text>首页</text>
+            <text>首页</text>
+         </scroll-view>
+     </view>
+  </header>
 </template>
 
 <script>
@@ -24,5 +34,17 @@ export default {
 </script>
 
 <style>
-
+header{
+  width: 100%;
+  height: 90rpx;
+  line-height: 90rpx;
+  background: pink;
+}
+.scroll-view_H{
+  white-space: nowrap;
+}
+.scroll-view_H text{
+  display: inline-block;
+  margin: 0 90rpx;
+}
 </style>
